0f95af0e8e902934955206457d5884b829400345
Federico Barresi <federico.barresi@evopro-ag.de>
2019-12-30 12:22:54 +0100

    added worker template

--- TFU002/TFU002.Service/Worker.cs
+++ TFU002/TFU002.Service/Worker.cs
@@ -10,20 +10,40 @@ namespace TFU002.Service
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
+        private readonly ILogger<Worker> logger;
 
         public Worker(ILogger<Worker> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            //Observe Beckhoff for run mode
+            // // Read symbols attributes
+            // // Create a subsriprion for evcery attribute Beckhoff => S7 and S7 => Beckhoff
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            //Load Settings
+
+            //Try Connect with all PLCs
+
+            await base.StartAsync(cancellationToken);
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            //Dispose all subscriptions
+
+            await base.StopAsync(cancellationToken);
         }
     }
 }
