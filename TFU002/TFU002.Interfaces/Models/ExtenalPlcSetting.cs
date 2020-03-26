b7b455a3de447db66e5e4a358d4a4f083ec2202a
Federico Barresi <federico.barresi@evopro-ag.de>
2020-03-26 20:56:09 +0100

    Changed default plc settings

--- TFU002/TFU002.Interfaces/Models/ExtenalPlcSetting.cs
+++ TFU002/TFU002.Interfaces/Models/ExtenalPlcSetting.cs
@@ -6,9 +6,9 @@ namespace TFU002.Interfaces.Models
     public class ExtenalPlcSetting
     {
         public string Name { get; set; } = "s7-300";
-        public string IpAddress { get; set; } = "10.30.10.50";
+        public string IpAddress { get; set; } = "127.0.0.1";
         public int Port { get; set; } = 102;
-        public int Rack { get; set; } = 0;
+        public int Rack { get; set; }
         public int Slot { get; set; } = 2;
     }
 }
